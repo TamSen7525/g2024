effafda345c3dc38df0c2444701e8d5e68b9268d
TamSen7525 <kadyev_edem@mail.ru>
2024-11-12 10:57:06 +0300

    g_4_3

--- g2024/routes/index.js
+++ g2024/routes/index.js
@@ -10,15 +10,28 @@ module.exports = router;
 
 /* Главная страница*/
 router.get('/cyberpunk-2077', function(req, res, next) {
-  res.send("<h1>CYBERPUNK 2077</h1>")
+  res.render('game',{
+    title: "CYBERPUNK2077",
+    picture: "images/cyberpunk-2077-back.png",
+    desc: "Примерьте на себя роль киберпанка Ви и приложите все усилия, чтобы сделать себе имя среди наёмников мегаполиса Найт-Сити, где власть, роскошь и модификации тела ценятся выше всего. Здесь рождаются легенды. Какую сложат о вас?"
+  })
   });
 
 /* Страница дополнения Phantom Liberty*/
 router.get('/phantom-liberty', function(req, res, next) {
-  res.send("<h1>PHANTOM LIBERTY</h1>")
+  res.render('game',{
+    title: "PHANTOM LIBERTY",
+    picture: "images/phantom-liberty-back.jpg",
+    desc: "«Призрачная свобода» — это сюжетное дополнение, привносящее элементы шпионского триллера в игру Cyberpunk 2077. Вернитесь к роли кибернетически усовершенствованного наёмника Ви и станьте шпионом НСША, чтобы спасти саму главу государства. Найдите союзников в Пёсьем городе, самом опасном районе Найт-Сити, чтобы распутать клубок шпионских и политических интриг. Сможете ли вы выполнить задание, от которого зависит ваша жизнь?"
+  })
   });
 
 /* Страница сериала Edgerunners*/
 router.get('/edgerunners', function(req, res, next) {
-  res.send("<h1>EDGERUNNERS</h1>")
+  res.render('game',{
+    title: "EDGERUNNERS",
+    picture: "images/edgerunners_back.jpg",
+    desc: "Cyberpunk: Edgerunners — это самостоятельная десятисерийная история о дите улиц, который пытается выжить в одержимом имплантами и новыми технологиями городе будущего."
+  })
   });
+  
