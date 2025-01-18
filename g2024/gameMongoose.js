12f32b81890daeae3c4252cbbb1efddefbffaa98
TamSen7525 <kadyev_edem@mail.ru>
2025-01-18 20:54:02 +0300

    g_7_2

--- g2024/gameMongoose.js
+++ g2024/gameMongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/gameMongoose2024');
+var schema = mongoose.Schema({ name: String })
 
-const game = mongoose.model('game', { name: String });
+schema.methods.g = function(){
+    console.log(this.name + " Класс!")}
+
+const game = mongoose.model('game', schema);
 
 const games = new game({ name: 'cyberpunk' });
-games.save().then(() => console.log('cyberpunk'));
+games.save().then(() => games.g());
+
