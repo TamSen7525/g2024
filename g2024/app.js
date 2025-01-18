cce6c2e34d1ae0d443d8e1f2b17870ac8e43af87
TamSen7525 <kadyev_edem@mail.ru>
2025-01-18 22:49:27 +0300

    g_8_2

--- g2024/app.js
+++ g2024/app.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://localhost/g2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var game = require('./routes/game');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/game', game);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
